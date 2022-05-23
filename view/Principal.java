1b9495f63f135e5e5d66c30ea050465404f243d9
Everton Santos <everton_santos_01@hotmail.com>
2022-05-23 12:00:33 -0300

    Primeiro Commit
    Primeiro Commit

---
+++ view/Principal.java
@@ -0,0 +1,9 @@
+package ibernateDDL1.view;
+
+import ESIIIHibernateDDL1.util.HibernateUtil;
+
+public class Principal {
+  public static void main(String[] args) {
+    HibernateUtil.getSessionFactory();
+  }
+}
